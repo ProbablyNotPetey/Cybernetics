27c9287e3924239c087f4f823dc726c8d65530ad
ProbablyNotPetey <petertvedt01@gmail.com>
2024-01-27 11:17:00 -0500

    mojang when they delete all their math classes:

--- src/main/java/com/vivi/cybernetics/client/gui/DiamondAbilityScreen.java
+++ src/main/java/com/vivi/cybernetics/client/gui/DiamondAbilityScreen.java
@@ -14,7 +14,7 @@ import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
-
+/*
 @Deprecated
 public class DiamondAbilityScreen extends Screen {
 
@@ -94,3 +94,4 @@ public class DiamondAbilityScreen extends Screen {
         }
     }
 }
+*/
