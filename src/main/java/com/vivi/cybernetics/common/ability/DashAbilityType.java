52adcb87e93f1ead433bc3733392df7f6cecb199
vertexcubed <95980769+vertexcubed@users.noreply.github.com>
2024-10-27 14:46:48 -0400

    Fix dash ability not always activating

--- src/main/java/com/vivi/cybernetics/common/ability/DashAbilityType.java
+++ src/main/java/com/vivi/cybernetics/common/ability/DashAbilityType.java
@@ -1,8 +1,11 @@
 package com.vivi.cybernetics.common.ability;
 
+import com.vivi.cybernetics.Cybernetics;
 import com.vivi.cybernetics.common.item.DashCyberwareItem;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import org.joml.Vector3d;
+import org.joml.Vector3f;
 
 public class DashAbilityType extends AbilityType {
 
@@ -18,8 +21,15 @@ public class DashAbilityType extends AbilityType {
     }
 
     public boolean canDash(Ability ability, Level level, Player player) {
-        if(player.getDeltaMovement().x == 0 && player.getDeltaMovement().z == 0) return false;
-        return !(ability.getCooldown() > -1) && (canDashMidair || (player.onGround() || player.onClimbable()));
+//        Cybernetics.LOGGER.info("Trying to dash? " + level.isClientSide);
+//        if(player.getDeltaMovement().x == 0 && player.getDeltaMovement().z == 0) return false;
+//        Cybernetics.LOGGER.debug("Player movenet: " + player.getDeltaMovement());
+        Vector3d movement = new Vector3d(player.getX(), player.getY(), player.getZ()).sub(new Vector3d(player.xOld, player.yOld, player.zOld));
+        if(Math.abs(movement.x) <= 0 && Math.abs(movement.z) <= 0) return false;
+//        Cybernetics.LOGGER.debug(new Vector3d(player.xOld, player.yOld, player.zOld) + " , " + new Vector3d(player.getX(), player.getY(), player.getZ()));
+        Cybernetics.LOGGER.info("Can dash: " + ((canDashMidair || (player.onGround() || player.onClimbable()))));
+        Cybernetics.LOGGER.debug("Cooldown: " + ability.getCooldown());
+        return (ability.getCooldown() == -1) && (canDashMidair || (player.onGround() || player.onClimbable()));
     }
 
     @Override
--- src/main/java/com/vivi/cybernetics/common/item/DashCyberwareItem.java
+++ src/main/java/com/vivi/cybernetics/common/item/DashCyberwareItem.java
@@ -1,5 +1,6 @@
 package com.vivi.cybernetics.common.item;
 
+import com.vivi.cybernetics.Cybernetics;
 import com.vivi.cybernetics.common.ability.AbilityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
@@ -13,7 +14,10 @@ public class DashCyberwareItem extends SimpleAbilityCyberwareItem {
     }
 
     public static void dash(Player player) {
-        Vec3 movement = player.getDeltaMovement();
-        player.setDeltaMovement(new Vec3(movement.x, 0.0, movement.z).normalize().scale(1.5));
+        Vec3 movement = new Vec3(player.getDeltaMovement().x, 0.0, player.getDeltaMovement().z);
+        Cybernetics.LOGGER.debug("Dashing!");
+        Cybernetics.LOGGER.debug("Movement: " + new Vec3(movement.x, 0.0, movement.z));
+        player.setDeltaMovement(movement.add(new Vec3(movement.x, 0.0, movement.z).normalize().scale(1.5)));
+//        player.hurtMarked = true;
     }
 }
